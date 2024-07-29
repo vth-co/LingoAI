6f230163172bf05010a0c67c5b680ab3d29484b0
Vu <vco.thien@gmail.com>
2024-07-29 12:06:51 -0700

    uninstalled tailwindcss

--- app/frontend/src/components/WelcomePage.js
+++ app/frontend/src/components/WelcomePage.js
@@ -3,8 +3,8 @@ import React from 'react'
 function WelcomePage () {
   return (
     <div>
-      <h1 className="text-red-400">Welcome!</h1>
-      <p className="text-white">This is a protected area accessible only by authenticated users.</p>
+      <h1>Welcome!</h1>
+      <p>This is a protected area accessible only by authenticated users.</p>
     </div>
   )
 }
