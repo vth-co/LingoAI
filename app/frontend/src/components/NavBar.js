b6b52e0f509ece11976b1f50b5dd3619e526c2b9
Carmen <carmenshiu07@gmail.com>
2024-08-03 11:52:58 -0700

    updated dropdown menu

--- app/frontend/src/components/NavBar.js
+++ app/frontend/src/components/NavBar.js
@@ -1,55 +1,54 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
-import { useDispatch, useSelector } from 'react-redux';
 import MaterialIcon from 'material-icons-react';
-import { Grid, Button, Typography, Box, Container } from "@mui/material";
-import { makeStyles } from '@mui/styles';
+import { Menu, Box, Container, MenuItem } from "@mui/material";
+import { styled } from '@mui/styles';
 
-const useStyles = makeStyles((theme) => ({
-  profileDropdown: {
-    position: 'absolute',
-    top: '120px',
-    right: '20px',
-    width: '200px',
-    marginTop: '5px',
-    backgroundColor: '#fff',
-    // boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
-    borderRadius: '8px',
-    border: '2px solid #000',
-    padding: '10px',
-    zIndex: 1000,
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color:
+      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+    boxShadow:
+      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    '& .MuiMenu-list': {
+      padding: '4px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+    },
   },
-  hidden: {
-    display: 'none'
-  }
 }));
 
+
 const NavBar = () => {
-  const classes = useStyles();
-  const dispatch = useDispatch();
-  const ulRef = useRef()
-  const [showMenu, setShowMenu] = useState(false);
-  const user = useSelector((state) => state.session.user);
-
-  const toggleMenu = (e) => {
-    e.stopPropagation();
-    setShowMenu(!showMenu)
-  }
-
-  useEffect(() => {
-    if (!showMenu || !ulRef.current) return;
-
-    const closeMenu = (e) => {
-      if (!ulRef.current.contains(e.target)) {
-        setShowMenu(false);
-      }
-    };
-
-    document.addEventListener('click', closeMenu);
-
-    return () => document.removeEventListener('click', closeMenu)
-  }, [showMenu])
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <nav>
@@ -74,31 +73,33 @@ const NavBar = () => {
             "&:hover": {
               cursor: "pointer"
             }
-
           }}
-          onClick={toggleMenu}
+          id="demo-customized-button"
+          aria-controls={open ? 'demo-customized-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          variant="contained"
+          disableElevation
+          onClick={handleClick}
         >
           <MaterialIcon icon="menu" size='medium' color='#f4f4f4' />
           <MaterialIcon icon="account_circle" size='medium' color='#f4f4f4' />
-
         </Box>
-        <div className={`${classes.profileDropdown} ${!showMenu ? classes.hidden : ''}`} ref={ulRef}>
-          <Box
-            sx={{
-              display: 'grid'
-            }}
-          >
-            <NavLink to='/login' exact={true} activeClassName='active'>
-              Log In
-            </NavLink>
-            <NavLink to='/sign-up' exact={true} activeClassName='active'>
-              Sign Up
-            </NavLink>
-          </Box>
-        </div>
-        {/* <LogoutButton /> */}
+        <StyledMenu
+          id="demo-customized-menu"
+          MenuListProps={{
+            'aria-labelledby': 'demo-customized-button',
+          }}
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+        >
+          <MenuItem><NavLink to='/login' exact={true} activeClassName='active'>
+            Log In
+          </NavLink></MenuItem>
+        </StyledMenu>
       </Container>
-    </nav >
+    </nav>
   );
 }
 
