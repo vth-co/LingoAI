34b0d46102b961155d6b1dd54dc1790dde114a4e
penny_w <penny0love.hotmail.com>
2024-08-03 02:35:13 -0700

    work on aiModel to add other prompts

--- app/frontend/src/controllers/aiController.js
+++ app/frontend/src/controllers/aiController.js
@@ -1,5 +1,5 @@
 const { addQuestionsToDB, getQuestionsByUserIdFromDB } = require('../services/aiService');
-const { generateQuestionsByAI } = require('../models/aiModel');
+const { generateGrammerQuestionsByAI } = require('../models/aiModel');
 const { options } = require('../routes/userRoutes');
 
 
@@ -21,11 +21,11 @@ const getAllQuestionsbyAI = async (req, res) => {
 // get question from AI and store in db
 const addCardQuestions = async (req, res) => {
     console.log("am i hitting get ai questions route")
-    const { user_native_language, user_level } = req.body
+    const { topic, user_native_language, user_level } = req.body
     try {
 
         //generate questionData for front
-        let questionData = await generateQuestionsByAI(user_native_language, user_level)
+        let questionData = await generateGrammerQuestionsByAI(topic, user_native_language, user_level)
         console.log("確認我拿到的questions是什麼: ", questionData, typeof (questionData))
 
         // //method 1- insert db 1 by 1
@@ -38,7 +38,7 @@ const addCardQuestions = async (req, res) => {
         //method 2- insert db with questionData
         const question_from_ai = await addQuestionsToDB("qX9q8i4wE24ohjSykFf8", { questionData });
 
-        res.status(201).json({ message: 'questions generated from ai added to db successfully!' });
+        res.status(201).json({ message: `questions generated from ai added to db successfully! ${question_from_ai.data()}` });
 
         // const { question, options, answer, explanation } = questionData
 
--- app/frontend/src/models/aiModel.js
+++ app/frontend/src/models/aiModel.js
@@ -1,9 +1,10 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { sendEmailVerification } = require("firebase/auth");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 
 
-const genAI = new GoogleGenerativeAI(API_KEY);
+const genAI = new GoogleGenerativeAI(KEY);
 
 
 
@@ -11,13 +12,33 @@ const genAI = new GoogleGenerativeAI(API_KEY);
 
 //generate questions from ai
 
-async function generateQuestionsByAI(native_language, level) {
+async function generateGrammerQuestionsByAI(topic, native_language, level) {
     console.log("am i hitting get ai functions and check the user level: ", native_language, level)
     // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", generationConfig: { responseMimeType: "application/json" } });
 
+    // Default prompt
+    let prompt = `You're an English teacher there're 3 student levels begineer, itermediate, advanced. Give me 3 unique fill-in-the-blank questions for ${topic} and mutiple choices in english,answer and explaination in ${native_language} for ${level} learner using this JSON schema: { "type":"array", "properties": {"question": "string",  "options": "array",  "answer": "string",  "explanation":"string"}}.`
+
+
+
+    if (topic === "Present Simple Tense") {
+        console.log("是否符合?")
+        prompt = `You're an English teacher there're 3 student levels begineer, itermediate, advanced. Give me 3 unique fill-in-the-blank questions for ${topic} and mutiple choices in english,answer and explaination in ${native_language} for ${level} learner using this JSON schema: { "type":"array", "properties": {"question": "string",  "options": "array",  "answer": "string",  "explanation":"string"}}.`
+    }
+    else if (topic === "Sentence Structure") {
+        console.log("是否符合 Sentence Structure?")
+        prompt = `You're an English teacher. There are 3 student levels: beginner, intermediate, and advanced. Create 3 questions for ${level} learners choose the sentence that follows the SVO structure, with options in english, answer and explanation in ${native_language} for ${level} learners using this JSON schema: { "type":"array", "properties": {"question": "string",  "options": "array",  "answer": "string",  "explanation":"string"}}.`
+    }
+    else if (topic === "Prepositions") {
+        console.log("是否符合 Prepositions?")
+        prompt = `You're an English teacher. create 3 practices for ${level} leaners to test if they understand Prepositions: In, on, at, under, over, beside. The practice includes a question and  4 options in English, answer and explanation in ${native_language} using this JSON schema: { "type":"array", "properties": {"question": "string",  "options": "array",  "answer": "string",  "explanation":"string"}}.`
+    }
+    else if (topic === "Possessive Pronouns") {
+        console.log("是否符合 Possessive Pronouns?")
+        prompt = `You're an English teacher. create 3 practices for ${level} leaners to test if they understand Possessive Pronouns. The practice includes a question and 4 options in English, answer and explanation in ${native_language} using this JSON schema: { "type":"array", "properties": {"question": "string",  "options": "array",  "answer": "string",  "explanation":"string"}}.`
+    }
     // const prompt = `You're an English teacher, there're 3 student levels - begineer, itermediate, advanced , I'm teaching ${native_language} as native language ${level} learner, provide a JavaScript Object Notation format includes 1 fill-in-the-blank question for present simple tense in english, mutiple choices ,answer and explaination in ${native_language} for ${level} learner.`
-    const prompt = `You're an English teacher there're 3 student levels begineer, itermediate, advanced. Give me 3 unique fill-in-the-blank questions for present simple tense and mutiple choices in english,answer and explaination in ${native_language} for ${level} learner using using this JSON schema: { "type":"array", "properties": {"question": "string",  "options": "array",  "answer": "string",  "explanation":"string"}}.`
 
 
     const result = await model.generateContent(prompt);
@@ -41,5 +62,5 @@ async function generateQuestionsByAI(native_language, level) {
 
 
 module.exports = {
-    generateQuestionsByAI
+    generateGrammerQuestionsByAI
 };
