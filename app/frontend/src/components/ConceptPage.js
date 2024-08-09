8a7f87682aae02d681bff9718f400b3843181001
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2024-08-09 18:40:50 -0400

    concepts page

--- app/frontend/src/components/ConceptPage.js
+++ app/frontend/src/components/ConceptPage.js
@@ -1,100 +1,159 @@
-import { Box, Button, Container, Grid, LinearProgress } from "@mui/material";
-import React from "react";
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchConcepts } from '../store/concepts'
+import { Box, Button, Container, Grid, LinearProgress } from '@mui/material'
 
-function ConceptPage() {
+//   return (
+//     <Container>
+//       <Box>
+//         <Box display="flex" flexDirection="column" alignItems="center">
+//           <h1>Select a Beginner Concept</h1>
+//           <p>
+//             These are the recommended concepts based on your selected
+//             proficiency level.
+//           </p>
+
+//           <p>Pass all the concepts to unlock the next proficiency level.</p>
+//         </Box>
+//         <Box px={50}>
+//           <LinearProgress
+//             variant="determinate"
+//             value={50}
+//             sx={{ height: 25 }}
+//           />
+//         </Box>
+//       </Box>
+
+//       <Grid container spacing={10} justifyContent="center" py={5}>
+//         <Grid item>
+//           <Button>
+//             <Box display="flex" flexDirection="column">
+//               <p>basic vocab</p>
+//               <p>explanation</p>
+//               <LinearProgress
+//                 variant="determinate"
+//                 value={50}
+//                 sx={{ height: 15 }}
+//               />
+//             </Box>
+//           </Button>
+//         </Grid>
+
+//         <Grid item>
+//           <Button>
+//             <Box display="flex" flexDirection="column">
+//               <p>basic vocab</p>
+//               <p>explanation</p>
+//               <LinearProgress
+//                 variant="determinate"
+//                 value={50}
+//                 sx={{ height: 15 }}
+//               />
+//             </Box>
+//           </Button>
+//         </Grid>
+//         <Grid item>
+//           <Button>
+//             <Box display="flex" flexDirection="column">
+//               <p>basic vocab</p>
+//               <p>explanation</p>
+//               <LinearProgress
+//                 variant="determinate"
+//                 value={50}
+//                 sx={{ height: 15 }}
+//               />
+//             </Box>
+//           </Button>
+//         </Grid>
+//       </Grid>
+//       <Grid container spacing={10} justifyContent="center" py={5}>
+//         <Grid item>
+//           <Button>
+//             <Box display="flex" flexDirection="column">
+//               <p>basic vocab</p>
+//               <p>explanation</p>
+//               <LinearProgress
+//                 variant="determinate"
+//                 value={50}
+//                 sx={{ height: 15 }}
+//               />
+//             </Box>
+//           </Button>
+//         </Grid>
+//         <Grid item>
+//           <Button>
+//             <Box display="flex" flexDirection="column">
+//               <p>basic vocab</p>
+//               <p>explanation</p>
+//               <LinearProgress
+//                 variant="determinate"
+//                 value={50}
+//                 sx={{ height: 15 }}
+//               />
+//             </Box>
+//           </Button>
+//         </Grid>
+//       </Grid>
+//     </Container>
+//   );
+// }
+
+// export default ConceptPage;
+
+function ConceptPage () {
+  const dispatch = useDispatch()
+  const {
+    items: concepts,
+    loading,
+    error
+  } = useSelector(state => state.concepts)
+
+  useEffect(() => {
+    dispatch(fetchConcepts())
+  }, [dispatch])
+
   return (
     <Container>
       <Box>
-        <Box display="flex" flexDirection="column" alignItems="center">
+        <Box display='flex' flexDirection='column' alignItems='center'>
           <h1>Select a Beginner Concept</h1>
           <p>
             These are the recommended concepts based on your selected
             proficiency level.
           </p>
-
           <p>Pass all the concepts to unlock the next proficiency level.</p>
         </Box>
         <Box px={50}>
           <LinearProgress
-            variant="determinate"
+            variant='determinate'
             value={50}
             sx={{ height: 25 }}
           />
         </Box>
       </Box>
 
-      <Grid container spacing={10} justifyContent="center" py={5}>
-        <Grid item>
-          <Button>
-            <Box display="flex" flexDirection="column">
-              <p>basic vocab</p>
-              <p>explanation</p>
-              <LinearProgress
-                variant="determinate"
-                value={50}
-                sx={{ height: 15 }}
-              />
-            </Box>
-          </Button>
+      {loading && <p>Loading concepts...</p>}
+      {error && <p>Error loading concepts: {error}</p>}
+      {!loading && !error && (
+        <Grid container spacing={10} justifyContent='center' py={5}>
+          {concepts.map(concept => (
+            <Grid item key={concept.id}>
+              <Button>
+                <Box display='flex' flexDirection='column'>
+                  <p>{concept.concept_name}</p> <p>{concept.level}</p>{' '}
+                  <LinearProgress
+                    variant='determinate'
+                    value={50}
+                    sx={{ height: 15 }}
+                  />
+                </Box>
+              </Button>
+            </Grid>
+          ))}
         </Grid>
-
-        <Grid item>
-          <Button>
-            <Box display="flex" flexDirection="column">
-              <p>basic vocab</p>
-              <p>explanation</p>
-              <LinearProgress
-                variant="determinate"
-                value={50}
-                sx={{ height: 15 }}
-              />
-            </Box>
-          </Button>
-        </Grid>
-        <Grid item>
-          <Button>
-            <Box display="flex" flexDirection="column">
-              <p>basic vocab</p>
-              <p>explanation</p>
-              <LinearProgress
-                variant="determinate"
-                value={50}
-                sx={{ height: 15 }}
-              />
-            </Box>
-          </Button>
-        </Grid>
-      </Grid>
-      <Grid container spacing={10} justifyContent="center" py={5}>
-        <Grid item>
-          <Button>
-            <Box display="flex" flexDirection="column">
-              <p>basic vocab</p>
-              <p>explanation</p>
-              <LinearProgress
-                variant="determinate"
-                value={50}
-                sx={{ height: 15 }}
-              />
-            </Box>
-          </Button>
-        </Grid>
-        <Grid item>
-          <Button>
-            <Box display="flex" flexDirection="column">
-              <p>basic vocab</p>
-              <p>explanation</p>
-              <LinearProgress
-                variant="determinate"
-                value={50}
-                sx={{ height: 15 }}
-              />
-            </Box>
-          </Button>
-        </Grid>
-      </Grid>
+      )}
     </Container>
-  );
+  )
 }
 
-export default ConceptPage;
+export default ConceptPage
